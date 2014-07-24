ab0b73e111b24e52d02b8a0664cc928cac052222
Ruslan Musagitov <ruslan.musagitov@aldigit.com>
2014-07-24 11:32:51 +0700

    Initial Commit

---
+++ 4KODE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  4KODE
+//
+//  Created by Ruslan on 7/24/14.
+//  Copyright (c) 2014 Ruslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 4KODE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  4KODE
+//
+//  Created by Ruslan on 7/24/14.
+//  Copyright (c) 2014 Ruslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
